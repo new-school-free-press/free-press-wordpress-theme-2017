16c00c4184bdeaae853a5416773ca1484c7f31f9
clairecoxo <cohec823@newschool.edu>
2017-10-22 13:01:28 -0400

    Cleanup

--- single/single-cat-news.php
+++ single/single-cat-news.php
@@ -1,72 +1,34 @@
 <?php
 /**
- * Template part for displaying posts
+ * The template for displaying all single posts
  *
- * @link https://codex.wordpress.org/Template_Hierarchy
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
  *
  * @package New_School_Free_Press
  */
 
-?>
-<script src="https://use.typekit.net/owo1sih.js"></script>
-<script>try{Typekit.load({ async: true });}catch(e){}</script>
+get_header(); ?>
 
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main">
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-<div class="article-content">
-	<header class="entry-header">
 		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
-        
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
+		while ( have_posts() ) : the_post();
+
+			get_template_part( 'template-parts/arts-culture', get_post_type() );
             
-                <?php if ( function_exists( 'coauthors_posts_links' ) ) {
-            coauthors_posts_links();
-        } else {
-            the_author_posts_link();
-        } ?>
+			the_post_navigation();
 
-		</div><!-- .entry-meta -->
+			// If comments are open or we have at least one comment, load up the comment template.
+			if ( comments_open() || get_comments_number() ) :
+				comments_template();
+			endif;
 
-		<?php
-		endif; ?>
-            <div class="entry-img">
-            <?php 
-        if (has_post_thumbnail()) {
-            the_post_thumbnail( 'full' );
-            }
-            ?>
-            </div>
-	</header><!-- .entry-header -->
-    
-	<div class="entry-content">
-		<?php
-			the_content( sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'new-school-free-press' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
-			) );
-
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'new-school-free-press' ),
-				'after'  => '</div>',
-			) );
+		endwhile; // End of the loop.
 		?>
-	</div><!-- .entry-content -->
-    </div>
-	<footer class="entry-footer">
-		<?php new_school_free_press_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-<?php the_ID(); ?> -->
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
